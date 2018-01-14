5573ffa16d5f00c2c4bea0a90ebf16f75e311022
Márk Bartha <barthamark@gmail.com>
2018-01-14 22:31:16 +0100

    Adding jQuery.DataTables resource
    --HG--
    branch : dev

---
+++ Constants/ResourceNames.cs
@@ -0,0 +1,7 @@
+﻿namespace Lombiq.DataTables.Constants
+{
+    public static class ResourceNames
+    {
+        public const string JQuery_DataTables = "JQuery.DataTables";
+    }
+}
---
+++ ResourceManifest.cs
@@ -0,0 +1,17 @@
+﻿using Orchard.UI.Resources;
+using static Lombiq.DataTables.Constants.ResourceNames;
+
+namespace Lombiq.DataTables
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            manifest.DefineScript(JQuery_DataTables).SetUrl("../Content/DataTables/dataTables.min.js", "../Content/DataTables/dataTables.js").SetDependencies("JQuery");
+
+            manifest.DefineStyle(JQuery_DataTables).SetUrl("../Content/DataTables/dataTables.min.css", "../Content/DataTables/dataTables.css");
+        }
+    }
+}
