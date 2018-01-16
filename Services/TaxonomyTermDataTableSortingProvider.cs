ed1d1fc3c10581144669493a7442d7b7339422e6
Benedek Farkas <benedek.farkas@lombiq.com>
2018-01-16 03:25:36 +0100

    Updating TaxonomyTermDataTableSortingProvider to be able sort items by Terms
    --HG--
    branch : issue/COLI-17

--- Services/TaxonomyTermDataTableSortingProvider.cs
+++ Services/TaxonomyTermDataTableSortingProvider.cs
@@ -1,44 +1,40 @@
 ﻿using Lombiq.DataTables.Constants;
 using Lombiq.DataTables.Models;
 using Orchard.Core.Title.Models;
-using Orchard.Projections.FieldTypeEditors;
-using Orchard.Taxonomies.Models;
-using Orchard.Utility.Extensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using static Lombiq.DataTables.Constants.DataTableColumnDefinitionSettingsKeys;
 
 namespace Lombiq.DataTables.Services
 {
     public class TaxonomyTermDataTableSortingProvider : IDataTableSortingProvider
     {
-        private const string AcceptedDataSource = DataTableDataSources.TaxonomyTerm;
-        
-
-        public bool CanSort(string dataSource) => dataSource == AcceptedDataSource;
+        public bool CanSort(string dataSource) => dataSource == DataTableDataSources.TaxonomyTerm;
 
         public void Sort(DataTableSortingContext context)
         {
             if (!CanSort(context.ColumnDefinition.DataSource)) return;
 
             var fieldName = context.ColumnDefinition[TaxonomyTermSorting.FieldName];
-            var aliasName = fieldName.ToSafeName();
+
+            if (string.IsNullOrEmpty(fieldName)) return;
+
+            var aliasName = "SortableTerms";
 
             context
                 .Query
                 .Where(
                     alias => alias
-                        .ContentPartRecord<TermsPartRecord>()
-                        .Property("Terms", aliasName),
+                        .ContentPartRecord<TitleSortableTermsPartRecord>()
+                        .Property(nameof(TitleSortableTermsPartRecord.Terms), aliasName),
                     predicate => predicate
-                        .Eq("Field", fieldName))
+                        .Eq(nameof(TitleSortableTermContentItem.Field), fieldName))
                 .OrderBy(
                     alias => alias.Named(aliasName),
                     order =>
                     {
-                        if (context.Direction == SortingDirection.Asc) order.Asc("TermRecord.Path");
-                        else order.Desc("TermRecord.Path");
+                        var property = $"{nameof(TitlePartRecord)}.{nameof(TitlePart.Title)}";
+
+                        if (context.Direction == SortingDirection.Asc) order.Asc(property);
+                        else order.Desc(property);
                     });
         }
     }
