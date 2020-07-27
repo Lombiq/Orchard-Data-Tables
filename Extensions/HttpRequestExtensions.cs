3eea9d3253bcc4f52173baf7497346b0c198aa2e
Dávid El-Saig <david.el-saig@lombiq.com>
2020-07-27 13:54:41 +0200

    Reimplement extension.

---
+++ Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace System.Web
+{
+    public static class HttpRequestExtensions
+    {
+        public static bool IsContentPickerRequest(this HttpRequest request) =>
+            UriHelper.GetEncodedUrl(request).Contains("/ContentPicker/");
+    }
+}
