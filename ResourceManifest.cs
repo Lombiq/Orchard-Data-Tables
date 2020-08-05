c03ed6942b319dec353284a690362509c52eaa67
Dávid El-Saig <david.el-saig@lombiq.com>
2020-08-05 18:47:04 +0200

    Fixed CDN.

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -19,24 +19,24 @@ namespace Lombiq.DataTables
                 .DefineScript(JQuery_DataTables)
                 .SetDependencies(JQuery)
                 .SetUrl("~/Lombiq.DataTables/vendors/datatables.net/jquery.dataTables.min.js", "~/Lombiq.DataTables/vendors/datatables.net/jquery.dataTables.js")
-                //.SetCdn("https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js", "https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js")
-                //.SetCdnIntegrity("sha384-L74JDRkaoB7PWnReNepwX6+kSckc13TJXrka4EerY9jxQxSDl0dTguSLcA7dEfq8","sha384-FeGVmTD/nb8R8suJjHKxL3iAigW2uFc536mNbyfM60EY3KH6wit0Jmgx0/QO2reU")
+                .SetCdn("https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js")
+                .SetCdnIntegrity("sha384-L74JDRkaoB7PWnReNepwX6+kSckc13TJXrka4EerY9jxQxSDl0dTguSLcA7dEfq8")
                 .SetVersion("1.10.20");
 
             manifest
                 .DefineScript(JQuery_DataTables_Bootstrap4)
                 .SetDependencies(JQuery, JQuery_DataTables)
                 .SetUrl("~/Lombiq.DataTables/vendors/datatables.net-bs4-js/dataTables.bootstrap4.min.js", "~/Lombiq.DataTables/vendors/datatables.net-bs4-js/dataTables.bootstrap4.js")
-                //.SetCdn("https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js", "https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.js")
-                //.SetCdnIntegrity("sha384-dsXH1jw5mvdtskz6tkzogTCdKWJv4k12j2BOHq3okVzlZiIsQhQXSh0I86ggUPPf","sha384-zBJRQUocgzK6hCN4Er9zN2l2fljUYdgHRSFXhzsgHwU2/HxnMPRr50a9Uevh/DBF")
+                .SetCdn("https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js")
+                .SetCdnIntegrity("sha384-dsXH1jw5mvdtskz6tkzogTCdKWJv4k12j2BOHq3okVzlZiIsQhQXSh0I86ggUPPf")
                 .SetVersion("1.10.20");
 
             manifest
                 .DefineStyle(JQuery_DataTables_Bootstrap4)
                 .SetDependencies(Bootstrap)
                 .SetUrl("~/Lombiq.DataTables/vendors/datatables.net-bs4-css/dataTables.bootstrap4.min.css", "~/Lombiq.DataTables/vendors/datatables.net-bs4-css/dataTables.bootstrap4.css")
-                //.SetCdn("https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css", "https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.css")
-                //.SetCdnIntegrity("sha384-EkHEUZ6lErauT712zSr0DZ2uuCmi3DoQj6ecNdHQXpMpFNGAQ48WjfXCE5n20W+R","sha384-0gIBab94tmRrgNHxYeuwE1hLc+W4Mv5SHxZeETbpK8TdKQlYOUwL0xcEgyw/Yc9U")
+                .SetCdn("https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css")
+                .SetCdnIntegrity("sha384-EkHEUZ6lErauT712zSr0DZ2uuCmi3DoQj6ecNdHQXpMpFNGAQ48WjfXCE5n20W+R")
                 .SetVersion("1.10.20");
 
             // Custom resources.
