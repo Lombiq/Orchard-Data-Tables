9de97298212a7d4066ba3bb2f5229fbd086fddbc
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-08-08 18:21:37 +0200

    Code cleanup

--- Constants/ContentPickerJavaScriptEvents.cs
+++ Constants/ContentPickerJavaScriptEvents.cs
@@ -1,9 +1,9 @@
-﻿namespace Lombiq.DataTables.Constants
+namespace Lombiq.DataTables.Constants
 {
     public static class ContentPickerJavaScriptEvents
     {
         private const string Prefix = "Lombiq.ContentPicker.";
 
-        public const string Change = nameof(Change);
+        public const string Change = Prefix + nameof(Change);
     }
 }
