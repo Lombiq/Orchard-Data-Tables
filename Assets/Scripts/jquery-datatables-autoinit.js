856d80ed752feb65f844b89b879107d2fceb4d06
Gábor Domonkos <gabor.domonkos@lombiq.com>
2020-08-11 17:32:30 +0200

    Removing empty lines

--- Assets/Scripts/jquery-datatables-autoinit.js
+++ Assets/Scripts/jquery-datatables-autoinit.js
@@ -1,10 +1,8 @@
 (function ($) {
     $(function () {
         $('table.data-table').each(function () {
-
             var options = this.getAttribute('data-options');
             $(this).dataTable(options ? JSON.parse(options) : undefined);
-
         })
     });
 }(jQuery));
