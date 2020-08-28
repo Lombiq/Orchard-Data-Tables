7d9b735d867716e64c1c10bb7ac19995debcb384
Krisztián Németh <krisztian.nemeth@lombiq.com>
2020-08-28 18:02:02 +0200

    Code styling and minor changes

--- Extensions/DataTableContentItemExtensions.cs
+++ Extensions/DataTableContentItemExtensions.cs
@@ -1,21 +1,27 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using System.Collections.Generic;
 
 namespace OrchardCore.ContentManagement
 {
     public static class DataTableContentItemExtensions
     {
-        public static string CreateEditLink(this ContentItem contentItem, LinkGenerator linkGenerator, HttpContext httpContext, string bootstrapTabType = null) =>
-            linkGenerator.GetUriByAction(httpContext,
-                "Edit",
-                "Admin",
-                new
-                {
-                    area = "OrchardCore.Contents",
-                    contentItemId = contentItem.ContentItemId,
-                    bootstraptab = bootstrapTabType == null
-                    ? null
-                    : $"tab-{bootstrapTabType}-{contentItem.ContentItemId}"
-                });
+        public static string CreateEditLink(
+            this ContentItem contentItem,
+            LinkGenerator linkGenerator,
+            HttpContext httpContext,
+            Dictionary<string, string> attributes = null,
+            string bootstrapTabType = null) =>
+                linkGenerator.GetUriByAction(httpContext,
+                    "Edit",
+                    "Admin",
+                    new
+                    {
+                        area = "OrchardCore.Contents",
+                        contentItemId = contentItem.ContentItemId,
+                        bootstraptab = attributes?["bootstraptab"] == null
+                        ? null
+                        : $"tab-{attributes?["bootstraptab"]}-{contentItem.ContentItemId}"
+                    });
     }
 }
