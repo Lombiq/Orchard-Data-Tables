0f2b6b4c6b467caa565a6bf08fcfecc47fbc3dff
Dávid El-Saig <david.el-saig@lombiq.com>
2020-09-14 06:09:09 +0200

    Handle text property sorting differently.

--- Models/ExportLink.cs
+++ Models/ExportLink.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Lombiq.DataTables.Models
 {
@@ -7,11 +6,6 @@ namespace Lombiq.DataTables.Models
     {
         public string Type { get => nameof(ExportLink); set {} }
         public string Url { get; set; }
-
-
-        // Make sure this appears first after serialization for sorting purposes. This is faster and easier than trying
-        // to type-check the resulting JObjects after the fact.
-        [JsonProperty(Order = -1)]
         public string Text { get; set; }
 
         public IDictionary<string, string> Attributes { get; set; } = new Dictionary<string, string>();
