5ef7035918c5463453cc2ba36ade735c20332763
Milán Keszthelyi <milan.keszthelyi@lombiq.com>
2020-11-17 17:31:35 +0100

    Data Tables ColReorder integration

--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -14,14 +14,16 @@ namespace Lombiq.DataTables
             manifest.DefineScript(JQuery_DataTables).SetUrl("../Content/DataTables/dataTables.min.js", "../Content/DataTables/dataTables.js").SetDependencies("jQuery");
             manifest.DefineScript(JQuery_DataTables_Plugins_Processing).SetUrl("../Content/DataTables/Plugins/processing().js").SetDependencies(JQuery_DataTables);
             manifest.DefineScript(JQuery_DataTables_Extensions_Responsive).SetUrl("../Content/DataTables/Extensions/Responsive/js/dataTables.responsive.min.js", "../Content/DataTables/Extensions/Responsive/js/dataTables.responsive.js").SetDependencies(JQuery_DataTables);
+            manifest.DefineScript(JQuery_DataTables_Extensions_ColReorder).SetUrl("../Content/DataTables/Extensions/ColReorder/js/dataTables.colReorder.min.js").SetDependencies(JQuery_DataTables);
 
             manifest.DefineStyle(JQuery_DataTables).SetUrl("../Content/DataTables/dataTables.min.css", "../Content/DataTables/dataTables.css");
             manifest.DefineStyle(JQuery_DataTables_Extensions_Responsive).SetUrl("../Content/DataTables/Extensions/Responsive/css/dataTables.responsive.min.css").SetDependencies(JQuery_DataTables);
+            manifest.DefineStyle(JQuery_DataTables_Extensions_ColReorder).SetUrl("../Content/DataTables/Extensions/ColReorder/css/colReorder.dataTables.min.css").SetDependencies(JQuery_DataTables);
 
             // Custom resources.
-            manifest.DefineScript(Lombiq_DataTables).SetUrl("lombiq-datatables.js").SetDependencies(Uri_Js, JQuery_DataTables_Plugins_Processing);
+            manifest.DefineScript(Lombiq_DataTables).SetUrl("lombiq-datatables.js").SetDependencies(Uri_Js, JQuery_DataTables_Plugins_Processing, JQuery_DataTables_Extensions_ColReorder);
             manifest.DefineScript(Lombiq_ContentPicker).SetUrl("lombiq-contentpicker.js").SetDependencies("jQueryColorBox");
-            
+
             manifest.DefineStyle(Lombiq_ContentPicker).SetDependencies("jQueryColorBox");
         }
     }
