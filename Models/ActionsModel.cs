81070c6a84d3b33c82918a52a29f3df8f7e14432
Dávid El-Saig <david.el-saig@lombiq.com>
2020-11-19 17:09:55 +0100

    Fix action filter when object is passed in.

--- Models/ActionsModel.cs
+++ Models/ActionsModel.cs
@@ -8,6 +8,8 @@ namespace Lombiq.DataTables.Models
 {
     public class ActionsModel
     {
+        private const string OrchardCoreContents = "OrchardCore.Contents";
+
         public string Id { get; set; }
         public IEnumerable<ExportLink> MenuItems { get; set; }
         public bool WithDefaults { get; set; } = true;
@@ -22,30 +24,50 @@ namespace Lombiq.DataTables.Models
             if (string.IsNullOrEmpty(Id)) WithDefaults = false;
             if (!WithDefaults) return MenuItems ?? Enumerable.Empty<ExportLink>();
 
-            var values = new
-            {
-                area = "OrchardCore.Contents",
-                contentItemId = Id,
-                returnUrl,
-            };
             var links = new List<ExportLink>
             {
-                new ExportLink(
-                    linkGenerator.GetUriByAction(context, "Edit", "Admin", values),
-                    stringLocalizer["Edit"].Value),
-                new ExportLink(
-                    linkGenerator.GetUriByAction(context, "Remove", "Admin", values),
-                    stringLocalizer["Delete"].Value,
-                    new Dictionary<string, string>
-                    {
-                        ["itemprop"] = "RemoveUrl UnsafeUrl",
-                        ["data-title"] = stringLocalizer["Delete"],
-                        ["data-message"] = stringLocalizer["Are you sure you want to delete this content item?"],
-                    }),
+                GetEditLink(Id, context, linkGenerator, stringLocalizer, returnUrl),
+                GetRemoveLink(Id, context, linkGenerator, stringLocalizer, returnUrl),
             };
 
             if (MenuItems != null) links.AddRange(MenuItems);
             return links;
+        }
+
+        public static ExportLink GetEditLink(
+            string contentItemId,
+            HttpContext context,
+            LinkGenerator linkGenerator,
+            IStringLocalizer<ActionsModel> stringLocalizer,
+            string returnUrl,
+            LocalizedString text = null)
+        {
+            var values = new { area = OrchardCoreContents, contentItemId, returnUrl };
+
+            return new ExportLink(
+                linkGenerator.GetUriByAction(context, "Edit", "Admin", values),
+                (text ?? stringLocalizer["Edit"]).Value);
+        }
+
+        public static ExportLink GetRemoveLink(
+            string contentItemId,
+            HttpContext context,
+            LinkGenerator linkGenerator,
+            IStringLocalizer<ActionsModel> stringLocalizer,
+            string returnUrl,
+            LocalizedString text = null)
+        {
+            var values = new { area = OrchardCoreContents, contentItemId, returnUrl };
+
+            return new ExportLink(
+                linkGenerator.GetUriByAction(context, "Remove", "Admin", values),
+                (text ?? stringLocalizer["Delete"]).Value,
+                new Dictionary<string, string>
+                {
+                    ["itemprop"] = "RemoveUrl UnsafeUrl",
+                    ["data-title"] = text ?? stringLocalizer["Delete"],
+                    ["data-message"] = stringLocalizer["Are you sure you want to delete this content item?"],
+                });
         }
     }
 }
