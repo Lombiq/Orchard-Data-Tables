8c88a188af6501ec744683e2b47dbd44fa5eff75
Krisztián Németh <krisztian.nemeth@lombiq.com>
2020-11-26 16:25:10 +0100

    Unit test culturinfo change

--- Tests/Lombiq.DataTables.Tests/UnitTests/Services/ExportTests.cs
+++ Tests/Lombiq.DataTables.Tests/UnitTests/Services/ExportTests.cs
@@ -177,9 +177,9 @@ namespace Lombiq.DataTables.Tests.UnitTests.Services
                 "Verify custom number formatting.",
                 new[]
                 {
-                    new object[] { 1, new DateTime(2020, 11, 26, 23, 42, 10).ToString(CultureInfo.InvariantCulture) },
-                    new object[] { 2, new DateTime(2020, 11, 26, 12, 42, 10).ToString(CultureInfo.InvariantCulture) },
-                    new object[] { 3, new DateTime(2020, 11, 26, 1, 42, 10).ToString(CultureInfo.InvariantCulture) },
+                    new object[] { 1, new DateTime(2020, 11, 26, 23, 42, 10).ToString(CultureInfo.CreateSpecificCulture("us-US")) },
+                    new object[] { 2, new DateTime(2020, 11, 26, 12, 42, 10).ToString(CultureInfo.CreateSpecificCulture("us-US")) },
+                    new object[] { 3, new DateTime(2020, 11, 26, 1, 42, 10).ToString(CultureInfo.CreateSpecificCulture("us-US")) },
                 },
                 new[] { ("Num", "Numbers", true), ("Time", "Time", true) },
                 "1,11/26/2020 11:42:10 PM;2,11/26/2020 12:42:10 PM;3,11/26/2020 1:42:10 AM".Split(';').Select(row => row.Split(',')).ToArray(),
