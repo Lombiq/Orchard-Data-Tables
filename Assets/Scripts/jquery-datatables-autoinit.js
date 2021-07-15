d860094f46d03b6ae90ebb714bc77123ab9f17b5
Dávid El-Saig <david.el-saig@lombiq.com>
2021-07-15 13:42:23 +0200

    JS and solution cleanup.

--- Assets/Scripts/jquery-datatables-autoinit.js
+++ Assets/Scripts/jquery-datatables-autoinit.js
@@ -1,8 +1,6 @@
-(function autoInit($) {
-    $(() => {
-        $('table.data-table').each(function dataTableEach() {
-            const options = this.getAttribute('data-options');
-            $(this).dataTable(options ? JSON.parse(options) : undefined);
-        });
+jQuery(($) => {
+    $('table.data-table').each(function dataTableEach() {
+        const options = this.getAttribute('data-options');
+        $(this).dataTable(options ? JSON.parse(options) : undefined);
     });
-}(jQuery));
+});
