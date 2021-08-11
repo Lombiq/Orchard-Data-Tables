f188f603ff47b35dc48c52422a70a0b5c0470c79
Bálint Aracsi <balint.aracsi@lombiq.com>
2021-08-11 20:20:17 +0200

    Using const with ternary instead of let.

--- Assets/Scripts/lombiq-datatables.js
+++ Assets/Scripts/lombiq-datatables.js
@@ -254,13 +254,12 @@
                         history.replaceState(createHistoryState(data), document.title);
                     }
 
-                    let requestData = $.extend({}, history.state.data);
+                    const requestData = !isNewRequest && (latestDraw ?? 0) === 0 
+                        ? $.extend({}, JSON.parse(localStorage.getItem("defaultRequest"))) 
+                        : $.extend({}, history.state.data);
                     if (!isNewRequest) requestData.draw = (latestDraw ?? 0) + 3;
                     if (requestData.draw === 1) {
                         localStorage.setItem("defaultRequest", JSON.stringify(requestData));
-                    }
-                    if (!isNewRequest && requestData.draw === 3) {
-                        requestData = $.extend({}, JSON.parse(localStorage.getItem("defaultRequest")));
                     }
 
                     const $wrapper = $element.closest('.dataTables_wrapper');
