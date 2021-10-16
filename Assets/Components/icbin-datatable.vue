b290160edcff2c55f2176eac5144c40c931c1dbc
Dávid El-Saig <david.el-saig@lombiq.com>
2021-10-16 21:17:39 +0200

    Table template, events, docs.

--- Assets/Components/icbin-datatable.vue
+++ Assets/Components/icbin-datatable.vue
@@ -3,13 +3,53 @@
         <div class="icbin-datatable-length-picker">
             {{ lengthPickerBefore }}
             <select v-model="showEntries">
-                <option v-for="length in lengths" :value="length">{{ length }}</option>
+                <option  v-for="lengthOption in lengths"
+                         v-model="length"
+                         :value="lengthOption">
+                    {{ lengthOption }}
+                </option>
             </select>
             {{ lengthPickerAfter }}
         </div>
         <div>
             <slot name="aboveHeader"></slot>
-            <table class="icbin-datatable-table">
+            <table class="icbin-datatable-table dataTable row-border stripe table data-table no-footer" role="grid">
+                <thead class="dataTable__header">
+                    <tr class="dataTable__headerRow" role="row">
+                        <th v-for="column in columns"
+                            class="dataTable__headerCell dataTable__cell sorting"
+                            scope="col"
+                            data-class-name="dataTable__cell"
+                            :data-orderable="column.orderable.toString()"
+                            :data-name="column.name"
+                            :data-data="column.name">
+                            <div class="dataTables_sizing">
+                                {{ column.text }}
+                            </div>
+                            <div class="icbin-datatable-sorter">
+                                <span class="(sort.name === column.name &&  self.sort.ascending) ? 'icbin-datatable-sorter-enabled' : ''">↑</span>
+                                <span class="(sort.name === column.name && !self.sort.ascending) ? 'icbin-datatable-sorter-enabled' : ''">↓</span>
+                            </div>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="dataTable__body">
+                    <tr v-for="(row, rowIndex) in sortedData"
+                        role="row"
+                        :class="'dataTable__row ' + (rowIndex % 2 ? 'even' : 'odd')">
+                        <td v-for="(cell, name) in row"
+                            :class="{ dataTable__cell: true, sorting_1: sort.name === name }">
+                            <component v-if="cell.component"
+                                       :is="cell.component.name"
+                                       v-bind="{ data: data, ...cell.component.value }"
+                                       @delete="deleteRow(row.$rowIndex, $event)"
+                                       @update="updateData($event)" />
+                            <a v-else-if="cell.href">{{ cell.text }}</a>
+                            <template v-else-if="cell.html" v-html="cell.html"></template>
+                            <span v-else>{{ cell.text }}</span>
+                        </td>
+                    </tr>
+                </tbody>
             </table>
             <div class="icbin-datatable-footer">
                 <div class="icbin-datatable-display-count">
@@ -41,9 +81,27 @@
 </template>
 
 <script>
+// events emitted:
+// - special(specialValue, cell): If the cell has a property called "special" (value is not null or
+//   undefined) this event is called inside the sortedData. This gives an opportunity for the
+//   client code to update the cell data (e.g. by setting the "component" and "hiddenInput"
+//   properties) with domain-specific behavior without having to edit this component.
+// - update(data): Sends the new desired value of the "data" property to the parent. Alternatively
+//   v-model can also be used.
+//
+// events received:
+// - delete(promptText): cell components may emit this event to signal a request to delete the row
+//   from the table's data. Optionally a non-empty String may be passed as event argument. If that
+//   happens, a prompt will be displayed with the given text to confirm with the user that they
+//   really want to remove the row.
+// - update(data): Same as above. The component may have a "data" property for this purpose.
 
 export default {
     name: 'icbin-datatable',
+    model: {
+        prop: 'data',
+        event: 'update'
+    },
     props: {
         text: {
             // Expected properties: lengthPicker, displayCount, previous, next.
@@ -63,6 +121,22 @@ export default {
             required: true,
         },
         data: {
+            // [
+            //   {
+            //     $rowIndex: Number,
+            //     columnName: {
+            //       // These can come from the server:
+            //       text: String,
+            //       html: String?,
+            //       sort: Object?,
+            //       href: String?,
+            //       special: String?,
+            //       // These can be set in JS code (e.g. with the "special" event):
+            //       component: { name: String?, props: Object }?
+            //       hiddenInput: { name: String, value: String }?
+            //     }
+            //   }
+            // ]
             type: Array,
             required: true,
         },
@@ -70,11 +144,18 @@ export default {
             type: Number,
             required: true,
         },
+        defaultSort: {
+            // { name: "columnName", ascending: true }
+            default: null,
+        }
     },
     data: function() {
         return {
             pageIndex: 0,
-
+            sort: {
+                name: null,
+                ascending: true,
+            },
         };
     },
     computed: {
@@ -105,11 +186,55 @@ export default {
 
             return range;
         },
+        sortedData(self) {
+            const sorted = self.data
+                .sort(function (row1, row2) {
+                    const sortable1 = row1[self.sort.name].sort ?? row1[self.sort.name].value;
+                    const sortable2 = row2[self.sort.name].sort ?? row2[self.sort.name].value;
+
+                    if (sortable1 < sortable2) return -1;
+                    if (sortable1 > sortable2) return 1;
+
+                    return 0;
+                });
+
+            const page = sorted.slice(self.pageIndex * self.length, self.length);
+
+            return page.map((row, rowPageOffset) => Object.fromEntries(
+                Object
+                    .entries(row)
+                    .map(function (cellPair) {
+                        let [name, cell] = cellPair;
+
+                        if (cell.special !== null && cell.special !== undefined) {
+                            // This lets the client code alter the cell.
+                            self.$emit('special', cell);
+                        }
+
+                        return [name, cell];
+                    })));
+        }
     },
     methods: {
         changePage(page) {
-
+            if (page >= 0 && page < this.total) this.pageIndex = page;
         },
+        deleteRow(rowIndex, promptText) {
+            if (!prompt(promptText)) return;
+            this.updateData(this.data.filter(row => row.$rowIndex !== rowIndex));
+        },
+        updateData(newData) { $this.$emit('update', newData); },
+    },
+    mounted: function() {
+        const self = this;
+
+        if (self.defaultSort) {
+            self.sort.name = self.defaultSort.name;
+            self.sort.ascending = self.defaultSort.ascending;
+        }
+        else {
+            self.sort.name = self.columns[0].name;
+        }
     },
 }
 </script>
