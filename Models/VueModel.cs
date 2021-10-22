4c1c2766375fcd68804e120564b7e8db6dcb3059
Dávid El-Saig <david.el-saig@lombiq.com>
2021-10-22 17:46:18 +0200

    TableToJsonAsync and fix formatting.

--- Models/VueModel.cs
+++ Models/VueModel.cs
@@ -1,27 +1,27 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using OrchardCore.ContentManagement;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Lombiq.DataTables.Models
 {
     public class VueModel
     {
-        [JsonProperty("text", NullValueHandling=NullValueHandling.Ignore)]
+        [JsonProperty("text", NullValueHandling = NullValueHandling.Ignore)]
         public string Text { get; set; }
 
-        [JsonProperty("html", NullValueHandling=NullValueHandling.Ignore)]
+        [JsonProperty("html", NullValueHandling = NullValueHandling.Ignore)]
         public string Html { get; set; }
 
-        [JsonProperty("sort", NullValueHandling=NullValueHandling.Ignore)]
+        [JsonProperty("sort", NullValueHandling = NullValueHandling.Ignore)]
         public object Sort { get; set; }
 
-        [JsonProperty("href", NullValueHandling=NullValueHandling.Ignore)]
+        [JsonProperty("href", NullValueHandling = NullValueHandling.Ignore)]
         public string Href { get; set; }
 
-        [JsonProperty("special", NullValueHandling=NullValueHandling.Ignore)]
+        [JsonProperty("special", NullValueHandling = NullValueHandling.Ignore)]
         public object Special { get; set; }
 
         public VueModel() { }
@@ -32,16 +32,11 @@ namespace Lombiq.DataTables.Models
             Href = href;
         }
 
-        public VueModel(IContent content)
+        public static async Task<JArray> TableToJsonAsync<T>(
+            IEnumerable<T> collection,
+            Func<T, Task<Dictionary<string, VueModel>>> select)
         {
-            Text = content.ContentItem.DisplayText;
-            Href = $"/Admin/Contents/ContentItems/{content.ContentItem.ContentItemId}/Edit";
-        }
-
-        public static JArray TableToJson<T>(IEnumerable<T> collection, Func<T, Dictionary<string, VueModel>> select)
-        {
-            var rows = collection
-                .Select(select)
+            var rows = (await collection.AwaitEachAsync(select))
                 .Select((row, rowIndex) =>
                 {
                     var castRow = row.ToDictionary(pair => pair.Key, pair => JToken.FromObject(pair.Value));
