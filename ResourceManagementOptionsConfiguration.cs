6acf62bc79b12bd8f70a186fa8e04e150341e0f7
Dávid El-Saig <david.el-saig@lombiq.com>
2021-11-28 04:08:51 +0100

    Revert ResourceManagementOptionsConfigurationBase.

--- ResourceManagementOptionsConfiguration.cs
+++ ResourceManagementOptionsConfiguration.cs
@@ -1,26 +1,26 @@
 using Lombiq.DataTables.Constants;
-using Lombiq.HelpfulLibraries.Libraries.ResourceManagement;
 using Microsoft.Extensions.Options;
 using OrchardCore.ResourceManagement;
 
 namespace Lombiq.DataTables
 {
-    public class ResourceManagementOptionsConfiguration : ResourceManagementOptionsConfigurationBase
+    public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
     {
         private const string WwwRoot = "~/Lombiq.DataTables/";
         private const string Vendors = WwwRoot + "vendors/";
         private const string Lombiq = WwwRoot + "lombiq/";
+        private static readonly ResourceManifest _manifest = new();
 
         static ResourceManagementOptionsConfiguration()
         {
             // jQuery.DataTables-related resources.
-            manifest
+            _manifest
                 .DefineScript(ResourceNames.UriJs)
                 .SetUrl(Vendors + "urijs/URI.min.js", Vendors + "urijs/URI.js")
                 .SetDependencies(ResourceNames.JQuery)
                 .SetVersion("1.19.2");
 
-            manifest
+            _manifest
                 .DefineScript(ResourceNames.DataTables.Library)
                 .SetDependencies(ResourceNames.JQuery)
                 .SetUrl(
@@ -34,7 +34,7 @@ namespace Lombiq.DataTables
                     "sha384-FeGVmTD/nb8R8suJjHKxL3iAigW2uFc536mNbyfM60EY3KH6wit0Jmgx0/QO2reU")
                 .SetVersion("1.10.20");
 
-            manifest
+            _manifest
                 .DefineScript(ResourceNames.DataTables.Buttons)
                 .SetDependencies(ResourceNames.JQuery, ResourceNames.DataTables.Library)
                 .SetUrl(
@@ -42,7 +42,7 @@ namespace Lombiq.DataTables
                     Vendors + "datatables.net-buttons/dataTables.buttons.js")
                 .SetVersion("1.6.3");
 
-            manifest
+            _manifest
                 .DefineScript(ResourceNames.DataTables.Bootstrap4)
                 .SetDependencies(ResourceNames.JQuery, ResourceNames.DataTables.Library)
                 .SetUrl(
@@ -56,7 +56,7 @@ namespace Lombiq.DataTables
                     "sha384-zBJRQUocgzK6hCN4Er9zN2l2fljUYdgHRSFXhzsgHwU2/HxnMPRr50a9Uevh/DBF")
                 .SetVersion("1.10.20");
 
-            manifest
+            _manifest
                 .DefineStyle(ResourceNames.DataTables.Bootstrap4)
                 .SetDependencies(ResourceNames.Bootstrap)
                 .SetUrl(
@@ -70,7 +70,7 @@ namespace Lombiq.DataTables
                     "sha384-0gIBab94tmRrgNHxYeuwE1hLc+W4Mv5SHxZeETbpK8TdKQlYOUwL0xcEgyw/Yc9U")
                 .SetVersion("1.10.20");
 
-            manifest
+            _manifest
                 .DefineScript(ResourceNames.DataTables.Bootstrap4Buttons)
                 .SetDependencies(ResourceNames.JQuery, ResourceNames.DataTables.Library, ResourceNames.DataTables.Buttons)
                 .SetUrl(
@@ -78,7 +78,7 @@ namespace Lombiq.DataTables
                     Vendors + "datatables.net-bs4-js/buttons.bootstrap4.js")
                 .SetVersion("1.6.3");
 
-            manifest
+            _manifest
                 .DefineStyle(ResourceNames.DataTables.Bootstrap4Buttons)
                 .SetDependencies(ResourceNames.Bootstrap, ResourceNames.DataTables.Bootstrap4, ResourceNames.DataTables.Buttons)
                 .SetUrl(
@@ -87,13 +87,13 @@ namespace Lombiq.DataTables
                 .SetVersion("1.6.3");
 
             // Custom resources.
-            manifest
+            _manifest
                 .DefineScript(ResourceNames.DataTables.AutoInit)
                 .SetDependencies(ResourceNames.DataTables.Bootstrap4, ResourceNames.DataTables.Bootstrap4Buttons)
                 .SetUrl(Lombiq + "jquery-datatables-autoinit.js")
                 .SetVersion("1.0");
 
-            manifest
+            _manifest
                 .DefineScript(ResourceNames.LombiqDataTables)
                 .SetDependencies(
                     ResourceNames.JQuery,
@@ -105,20 +105,22 @@ namespace Lombiq.DataTables
                 .SetUrl(Lombiq + "lombiq-datatables.js")
                 .SetVersion("1.0");
 
-            manifest
+            _manifest
                 .DefineScript(ResourceNames.LombiqContentPicker)
                 .SetUrl(Lombiq + "lombiq-contentpicker.js")
                 .SetDependencies("jQueryColorBox");
 
-            manifest
+            _manifest
                 .DefineStyle(ResourceNames.LombiqContentPicker)
                 .SetDependencies("jQueryColorBox");
 
-            manifest
+            _manifest
                 .DefineScript(ResourceNames.ICantBelieveItsNotDataTable)
                 .SetUrl(Lombiq + "icbin-datatable.js")
                 .SetDependencies("vuejs")
                 .SetVersion("1.0.2");
         }
+
+        public void Configure(ResourceManagementOptions options) => options.ResourceManifests.Add(_manifest);
     }
 }
