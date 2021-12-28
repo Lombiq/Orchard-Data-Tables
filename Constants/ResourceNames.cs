624ae3e0a249f6850caf07ffcd5b6912c854be4c
Ádám Jakab <adam.jakab@lombiq.com>
2021-12-29 00:28:21 +0100

    Adding datetime-moment plugin

--- Constants/ResourceNames.cs
+++ Constants/ResourceNames.cs
@@ -5,6 +5,7 @@
         public const string Uri_Js = "URI.js";
         public const string JQuery_DataTables = "jQuery.DataTables";
         public const string JQuery_DataTables_Plugins_Processing = JQuery_DataTables + ".Plugins.Processing";
+        public const string JQuery_DataTables_Plugins_DateTime_Moment = JQuery_DataTables + ".Plugins.DateTime.Moment";
         public const string JQuery_DataTables_Extensions_Responsive = JQuery_DataTables + ".Extensions.Responsive";
         public const string JQuery_DataTables_Extensions_ColReorder = JQuery_DataTables + ".Extensions.ColReorder";
         public const string Lombiq_DataTables = "Lombiq.DataTables";
--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -13,6 +13,7 @@ namespace Lombiq.DataTables
             manifest.DefineScript(Uri_Js).SetUrl("../Content/URI.js/URI.min.js", "../Content/URI.js/URI.js").SetDependencies("jQuery");
             manifest.DefineScript(JQuery_DataTables).SetUrl("../Content/DataTables/dataTables.min.js", "../Content/DataTables/dataTables.js").SetDependencies("jQuery");
             manifest.DefineScript(JQuery_DataTables_Plugins_Processing).SetUrl("../Content/DataTables/Plugins/processing().js").SetDependencies(JQuery_DataTables);
+            manifest.DefineScript(JQuery_DataTables_Plugins_DateTime_Moment).SetUrl("../Content/DataTables/Plugins/datetime-moment.js").SetCdn("//cdn.datatables.net/plug-ins/1.11.3/sorting/datetime-moment.js").SetVersion("1.11.3").SetDependencies(JQuery_DataTables, "Moment");
             manifest.DefineScript(JQuery_DataTables_Extensions_Responsive).SetUrl("../Content/DataTables/Extensions/Responsive/js/dataTables.responsive.min.js", "../Content/DataTables/Extensions/Responsive/js/dataTables.responsive.js").SetDependencies(JQuery_DataTables);
             manifest.DefineScript(JQuery_DataTables_Extensions_ColReorder).SetUrl("../Content/DataTables/Extensions/ColReorder/js/dataTables.colReorder.min.js").SetDependencies(JQuery_DataTables);
 
@@ -21,7 +22,7 @@ namespace Lombiq.DataTables
             manifest.DefineStyle(JQuery_DataTables_Extensions_ColReorder).SetUrl("../Content/DataTables/Extensions/ColReorder/css/colReorder.dataTables.min.css").SetDependencies(JQuery_DataTables);
 
             // Custom resources.
-            manifest.DefineScript(Lombiq_DataTables).SetUrl("lombiq-datatables.js").SetDependencies(Uri_Js, JQuery_DataTables_Plugins_Processing, JQuery_DataTables_Extensions_ColReorder);
+            manifest.DefineScript(Lombiq_DataTables).SetUrl("lombiq-datatables.js").SetDependencies(Uri_Js, JQuery_DataTables_Plugins_Processing, JQuery_DataTables_Plugins_DateTime_Moment, JQuery_DataTables_Extensions_ColReorder);
             manifest.DefineScript(Lombiq_ContentPicker).SetUrl("lombiq-contentpicker.js").SetDependencies("jQueryColorBox");
 
             manifest.DefineStyle(Lombiq_ContentPicker).SetDependencies("jQueryColorBox");
