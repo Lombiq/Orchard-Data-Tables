4458ef51aa3220363403c1d98d1f0bbbeb926299
Dávid El-Saig <david.el-saig@lombiq.com>
2022-01-17 19:41:30 +0100

    Remove unnecessary routing boilerplate.

--- Lombiq.DataTables.Samples/Startup.cs
+++ Lombiq.DataTables.Samples/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using Fluid;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
 
@@ -11,19 +7,6 @@ namespace Lombiq.DataTables.Samples
     {
         public override void ConfigureServices(IServiceCollection services)
         {
-        }
-
-        public override void Configure(
-            IApplicationBuilder builder,
-            IEndpointRouteBuilder routes,
-            IServiceProvider serviceProvider)
-        {
-            routes.MapAreaControllerRoute(
-                name: "Home",
-                areaName: "Lombiq.DataTables.Samples",
-                pattern: "Home/Index",
-                defaults: new { controller = "Home", action = "Index" }
-            );
         }
     }
 }
