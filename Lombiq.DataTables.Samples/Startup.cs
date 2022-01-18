94ed160d654a670bb76ddd6c843fb5b25d35b5e4
Dávid El-Saig <david.el-saig@lombiq.com>
2022-01-18 17:44:37 +0100

    Sample project

---
+++ Lombiq.DataTables.Samples/Startup.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace Lombiq.DataTables.Samples
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+    }
+}
