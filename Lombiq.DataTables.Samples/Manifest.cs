c969627f4d916ece5b77eabbd9acc52661af08c4
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-02-21 21:52:11 +0100

    Updating manifest version numbers

--- Lombiq.DataTables.Samples/Manifest.cs
+++ Lombiq.DataTables.Samples/Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "Lombiq Data Tables - Samples",
     Author = "Lombiq Technologies",
     Website = "https://github.com/Lombiq/Orchard-Data-Tables",
-    Version = "2.1.0",
+    Version = "2.1.3",
     Description = "Samples for Lombiq Data Tables.",
     Category = "Development",
     Dependencies = new[] { Lombiq.DataTables.Constants.FeatureIds.DataTables }
