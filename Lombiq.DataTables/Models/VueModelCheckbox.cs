20c8a95c6dfd21593adb261fd2c92bec567e3772
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-03-31 22:47:53 +0200

    Suppressing inconsistent analyzer violation

--- Lombiq.DataTables/Models/VueModelCheckbox.cs
+++ Lombiq.DataTables/Models/VueModelCheckbox.cs
@@ -1,10 +1,18 @@
 using Newtonsoft.Json;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Lombiq.DataTables.Models;
 
 // This class is used in <icbin-datatable>.
 public class VueModelCheckbox
 {
+    // While the warning doesn't show up in VS it does with dotnet build, but inconsistently.
+#pragma warning disable IDE0079 // Remove unnecessary suppression
+    [SuppressMessage(
+        "Performance",
+        "CA1822:Mark members as static",
+        Justification = "It's necessary to be instance-level for JSON serialization.")]
+#pragma warning restore IDE0079 // Remove unnecessary suppression
     [JsonProperty("type")]
     public string Type => "checkbox";
 
