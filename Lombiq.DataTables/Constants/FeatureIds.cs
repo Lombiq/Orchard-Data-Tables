6f47c383d371dddd422c53bcaece1274226ca18b
Sára El-Saig <sara.el-saig@lombiq.com>
2024-02-02 20:49:05 +0100

    Use "Area" constant where relevant.

--- Lombiq.DataTables/Constants/FeatureIds.cs
+++ Lombiq.DataTables/Constants/FeatureIds.cs
@@ -2,5 +2,7 @@ namespace Lombiq.DataTables.Constants;
 
 public static class FeatureIds
 {
-    public const string DataTables = "Lombiq.DataTables";
+    public const string Area = "Lombiq.DataTables";
+
+    public const string DataTables = Area;
 }
