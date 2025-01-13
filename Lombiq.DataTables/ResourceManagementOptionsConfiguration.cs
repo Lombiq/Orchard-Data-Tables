243da5988a8f9595a4701d76beb1788c0bbbdf07
Sára El-Saig <sara.el-saig@lombiq.com>
2025-01-13 13:16:10 +0100

    Update resource definitions.

--- Lombiq.DataTables/ResourceManagementOptionsConfiguration.cs
+++ Lombiq.DataTables/ResourceManagementOptionsConfiguration.cs
@@ -27,8 +27,8 @@ public class ResourceManagementOptionsConfiguration : IConfigureOptions<Resource
                 Vendors + "datatables.net/jquery.dataTables.min.js",
                 Vendors + "datatables.net/jquery.dataTables.js")
             .SetCdn(
-                "https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js",
-                "https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js")
+                "https://cdn.datatables.net/2.2.1/js/dataTables.min.js",
+                "https://cdn.datatables.net/2.2.1/js/dataTables.js")
             .SetCdnIntegrity(
                 "sha384-L74JDRkaoB7PWnReNepwX6+kSckc13TJXrka4EerY9jxQxSDl0dTguSLcA7dEfq8",
                 "sha384-FeGVmTD/nb8R8suJjHKxL3iAigW2uFc536mNbyfM60EY3KH6wit0Jmgx0/QO2reU")
@@ -43,53 +43,53 @@ public class ResourceManagementOptionsConfiguration : IConfigureOptions<Resource
             .SetVersion("1.6.3");
 
         _manifest
-            .DefineScript(ResourceNames.DataTables.Bootstrap4)
+            .DefineScript(ResourceNames.DataTables.Bootstrap5)
             .SetDependencies(ResourceNames.JQuery, ResourceNames.DataTables.Library)
             .SetUrl(
-                Vendors + "datatables.net-bs4-js/dataTables.bootstrap4.min.js",
-                Vendors + "datatables.net-bs4-js/dataTables.bootstrap4.js")
+                Vendors + "datatables.net-bs5-js/dataTables.bootstrap5.min.js",
+                Vendors + "datatables.net-bs5-js/dataTables.bootstrap5.js")
             .SetCdn(
-                "https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js",
-                "https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.js")
+                "https://cdn.datatables.net/2.2.1/js/dataTables.bootstrap5.min.js",
+                "https://cdn.datatables.net/2.2.1/js/dataTables.bootstrap5.js")
             .SetCdnIntegrity(
                 "sha384-dsXH1jw5mvdtskz6tkzogTCdKWJv4k12j2BOHq3okVzlZiIsQhQXSh0I86ggUPPf",
                 "sha384-zBJRQUocgzK6hCN4Er9zN2l2fljUYdgHRSFXhzsgHwU2/HxnMPRr50a9Uevh/DBF")
             .SetVersion("1.10.20");
 
         _manifest
-            .DefineStyle(ResourceNames.DataTables.Bootstrap4)
+            .DefineStyle(ResourceNames.DataTables.Bootstrap5)
             .SetDependencies(ResourceNames.Bootstrap)
             .SetUrl(
-                Vendors + "datatables.net-bs4-css/dataTables.bootstrap4.min.css",
-                Vendors + "datatables.net-bs4-css/dataTables.bootstrap4.css")
+                Vendors + "datatables.net-bs5-css/dataTables.bootstrap5.min.css",
+                Vendors + "datatables.net-bs5-css/dataTables.bootstrap5.css")
             .SetCdn(
-                "https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css",
-                "https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.css")
+                "https://cdn.datatables.net/2.2.1/css/dataTables.bootstrap5.min.css",
+                "https://cdn.datatables.net/2.2.1/css/dataTables.bootstrap5.css")
             .SetCdnIntegrity(
                 "sha384-EkHEUZ6lErauT712zSr0DZ2uuCmi3DoQj6ecNdHQXpMpFNGAQ48WjfXCE5n20W+R",
                 "sha384-0gIBab94tmRrgNHxYeuwE1hLc+W4Mv5SHxZeETbpK8TdKQlYOUwL0xcEgyw/Yc9U")
             .SetVersion("1.10.20");
 
         _manifest
-            .DefineScript(ResourceNames.DataTables.Bootstrap4Buttons)
+            .DefineScript(ResourceNames.DataTables.Bootstrap5Buttons)
             .SetDependencies(ResourceNames.JQuery, ResourceNames.DataTables.Library, ResourceNames.DataTables.Buttons)
             .SetUrl(
-                Vendors + "datatables.net-bs4-js/buttons.bootstrap4.min.js",
-                Vendors + "datatables.net-bs4-js/buttons.bootstrap4.js")
+                Vendors + "datatables.net-bs5-js/buttons.bootstrap5.min.js",
+                Vendors + "datatables.net-bs5-js/buttons.bootstrap5.js")
             .SetVersion("1.6.3");
 
         _manifest
-            .DefineStyle(ResourceNames.DataTables.Bootstrap4Buttons)
-            .SetDependencies(ResourceNames.Bootstrap, ResourceNames.DataTables.Bootstrap4, ResourceNames.DataTables.Buttons)
+            .DefineStyle(ResourceNames.DataTables.Bootstrap5Buttons)
+            .SetDependencies(ResourceNames.Bootstrap, ResourceNames.DataTables.Bootstrap5, ResourceNames.DataTables.Buttons)
             .SetUrl(
-                Vendors + "datatables.net-bs4-css/buttons.bootstrap4.min.css",
-                Vendors + "datatables.net-bs4-css/buttons.bootstrap4.css")
+                Vendors + "datatables.net-bs5-css/buttons.bootstrap5.min.css",
+                Vendors + "datatables.net-bs5-css/buttons.bootstrap5.css")
             .SetVersion("1.6.3");
 
         // Custom resources.
         _manifest
             .DefineScript(ResourceNames.DataTables.AutoInit)
-            .SetDependencies(ResourceNames.DataTables.Bootstrap4, ResourceNames.DataTables.Bootstrap4Buttons)
+            .SetDependencies(ResourceNames.DataTables.Bootstrap5, ResourceNames.DataTables.Bootstrap5Buttons)
             .SetUrl(Lombiq + "jquery-datatables-autoinit.js")
             .SetVersion("1.0");
 
@@ -99,9 +99,9 @@ public class ResourceManagementOptionsConfiguration : IConfigureOptions<Resource
                 ResourceNames.JQuery,
                 ResourceNames.UriJs,
                 ResourceNames.DataTables.Library,
-                ResourceNames.DataTables.Bootstrap4,
+                ResourceNames.DataTables.Bootstrap5,
                 ResourceNames.DataTables.Buttons,
-                ResourceNames.DataTables.Bootstrap4Buttons)
+                ResourceNames.DataTables.Bootstrap5Buttons)
             .SetUrl(Lombiq + "lombiq-datatables.js")
             .SetVersion("1.0");
 
