234a773e3f098019fc0363b26daf83f7a90018a3
Sára El-Saig <sara.el-saig@lombiq.com>
2025-01-14 14:51:07 +0100

    Update waiting logic in UI testing.

--- Lombiq.DataTables.Tests.UI/Extensions/UITestContextExtensions.cs
+++ Lombiq.DataTables.Tests.UI/Extensions/UITestContextExtensions.cs
@@ -1,12 +1,12 @@
 using Lombiq.DataTables.Samples.Controllers;
 using Lombiq.DataTables.Services;
-using Lombiq.DataTables.Tests.UI.Models;
 using Lombiq.HelpfulLibraries.Common.Utilities;
 using Lombiq.HelpfulLibraries.OrchardCore.Mvc;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
-using Shouldly;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Lombiq.DataTables.Tests.UI.Extensions;
@@ -48,8 +48,14 @@ public static class UITestContextExtensions
 
     public static async Task ClickAndWaitForTableChangeAsync(this UITestContext context, By selector)
     {
-        var state = new TableDrawState(context);
+        var state = GetTableState(context);
         await context.ClickReliablyOnAsync(selector);
-        state.Wait();
+        context.DoWithRetriesOrFail(() => GetTableState(context) != state);
     }
+
+    private static string GetTableState(UITestContext context) =>
+        context
+            .GetAll(By.CssSelector(".dataTableWrapper td"))
+            .Select(element => element.Text.Trim())
+            .Join();
 }
