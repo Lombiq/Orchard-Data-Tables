4f0bd57a981efff1a258b57b11c8113e193b68dc
Sára El-Saig <sara.el-saig@lombiq.com>
2025-01-14 12:37:43 +0100

    same

--- Lombiq.DataTables.Tests.UI/Models/TableDrawState.cs
+++ Lombiq.DataTables.Tests.UI/Models/TableDrawState.cs
@@ -16,7 +16,7 @@ public class TableDrawState : IWebContentState
     public TableDrawState(UITestContext context, By wrapper = null)
     {
         _context = context;
-        _by = wrapper ?? By.ClassName("dataTable_wrapper");
+        _by = wrapper ?? By.CssSelector("*[data-draw]");
         _draw = GetCurrentDraw();
     }
 
