9327135705ffba5c529cf7f8d01f83caa50140d7
Sára El-Saig <sara.el-saig@lombiq.com>
2025-01-20 12:35:01 +0100

    Fix the bug described in #183.

--- Lombiq.DataTables/Services/ExcelDataTableExportService.cs
+++ Lombiq.DataTables/Services/ExcelDataTableExportService.cs
@@ -67,6 +67,13 @@ public class ExcelDataTableExportService : IDataTableExportService
         string error = null,
         IDictionary<int, string> customNumberFormat = null)
     {
+        // The worksheet name must be a non-empty string no longer than 31 characters. If you give it a longer name in
+        // Microsoft Excel or LibreOffice Calc and then save it in the XLSX (Excel 2007-365) format, the result will
+        // also be truncated when reopened so this is an authentic behavior.
+        worksheetName = string.IsNullOrWhiteSpace(worksheetName)
+            ? localizer["Untitled Worksheet"]
+            : worksheetName.Trim()[..31];
+
         using var workbook = new XLWorkbook();
         var worksheet = workbook.Worksheets.Add(worksheetName);
 
